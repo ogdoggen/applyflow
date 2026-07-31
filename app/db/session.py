5891d949df94090905d7895ad728a44fd52e1ada
Daniil Agafonov <doggen999@gmail.com>
2026-08-01 01:45:16 +0400

    practice with async sessions

--- app/db/session.py
+++ app/db/session.py
@@ -1,10 +1,16 @@
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from app.config import settings
+from sqlalchemy.ext.asyncio import (
+    AsyncSession,
+    async_sessionmaker,
+    create_async_engine,
+)
 
 engine = create_async_engine(settings.database_url, pool_pre_ping = True, echo = settings.debug)
 
-session_factory = async_sessionmaker(bind=engine, expire_on_commit=False)
-
+session_factory: async_sessionmaker[AsyncSession] = async_sessionmaker(
+    bind=engine,
+    expire_on_commit=False,
+)
 async def get_db():
     db = session_factory()
     try:
