5b916156e71563e929a587959497d9ac05254c32
Daniil Agafonov <doggen999@gmail.com>
2026-08-21 03:20:43 +0400

    feat: authorize vacancy access by owner

--- app/routes/vacancies.py
+++ app/routes/vacancies.py
@@ -2,30 +2,30 @@ from fastapi import APIRouter
 
 from app.schemas.vacancies import Vacancy, VacancyCreate, VacancyRead, VacancyUpdate, VacancyStatus
 from ..services import vacancy_service
-from app.dependencies import db_session
+from app.dependencies import db_session, CurrentUser
 
 router = APIRouter(prefix = "/vacancies", tags = ["vacancies"])
 
 
 
 @router.get("", response_model=list[VacancyRead])
-async def list_vacancies(session : db_session, status : VacancyStatus | None = None, company : str | None = None,
+async def list_vacancies(session : db_session, current_user : CurrentUser, status : VacancyStatus | None = None, company : str | None = None,
                          limit: int | None = None, offset : int | None = None):
-    return await vacancy_service.list_vacancies(session = session, status = status, company = company, limit = limit, offset = offset)
+    return await vacancy_service.list_vacancies(session = session, current_user=current_user, status = status, company = company, limit = limit, offset = offset)
 
 @router.get("/{vacancy_id}", response_model=VacancyRead)
-async def return_vacancy(session : db_session, vacancy_id:int):
-    return await vacancy_service.return_vacancy(session, vacancy_id)
+async def return_vacancy(session : db_session, current_user : CurrentUser, vacancy_id:int):
+    return await vacancy_service.return_vacancy(session, current_user, vacancy_id)
 
 @router.post("", response_model= VacancyRead, status_code=201)
-async def create_vacancy(vacancy : VacancyCreate, session : db_session):
-    return await vacancy_service.create_vacancy(session = session, vacancy = vacancy)
+async def create_vacancy(vacancy : VacancyCreate, session : db_session, current_user : CurrentUser):
+    return await vacancy_service.create_vacancy(session = session, current_user=current_user, vacancy = vacancy)
 
 
 @router.patch("/{vacancy_id}", status_code=200, response_model=VacancyRead)
-async def update_vacancy(session : db_session, new_data: VacancyUpdate, vacancy_id: int):
-    return await vacancy_service.update_vacancy(session, new_data, vacancy_id)
+async def update_vacancy(session : db_session, current_user : CurrentUser, new_data: VacancyUpdate, vacancy_id: int):
+    return await vacancy_service.update_vacancy(session, current_user, new_data, vacancy_id)
 
 @router.delete("/{vacancy_id}", status_code=204)
-async def delete_vacancy(session : db_session ,vacancy_id : int):
-    return await vacancy_service.delete_vacancy(session, vacancy_id)
+async def delete_vacancy(session : db_session, current_user : CurrentUser,vacancy_id : int):
+    return await vacancy_service.delete_vacancy(session, current_user, vacancy_id)
--- app/services/vacancy_service.py
+++ app/services/vacancy_service.py
@@ -5,20 +5,21 @@ from sqlalchemy.exc import SQLAlchemyError
 
 from app.schemas.vacancies import Vacancy, VacancyCreate, VacancyRead, VacancyUpdate, VacancyStatus
 from app.models.vacancy import VacancyModel
+from app.models.user import UserModel
 
-async def find_vacancy_or_404(session : AsyncSession, vacancy_id : int):
-    vacancy = await session.get(VacancyModel, vacancy_id)
+async def find_owned_vacancy_or_404(session : AsyncSession, current_user : UserModel, vacancy_id : int):
+    # vacancy = await session.get(VacancyModel, vacancy_id)
+    stmt = select(VacancyModel).where(VacancyModel.owner_id == current_user.id, VacancyModel.id == vacancy_id)
+    vacancy = await session.scalar(stmt)
     if vacancy is None:
         raise HTTPException(status_code=404, detail="vacancy not found")
     return vacancy
 
 
-async def list_vacancies(session : AsyncSession, status : VacancyStatus | None = None, company : str | None = None,
+async def list_vacancies(session : AsyncSession, current_user : UserModel, status : VacancyStatus | None = None, company : str | None = None,
                          limit: int | None = None, offset : int | None = None):
 
-    async with (session.begin()):
-
-        stmt = (select(VacancyModel))
+        stmt = select(VacancyModel).where(VacancyModel.owner_id == current_user.id)
         if status is not None:
             stmt = stmt.where(VacancyModel.status == status)
 
@@ -31,15 +32,16 @@ async def list_vacancies(session : AsyncSession, status : VacancyStatus | None =
         result = await session.scalars(stmt)
         return result.all()
 
-async def return_vacancy(session : AsyncSession, vacancy_id : int):
-    return await find_vacancy_or_404(session, vacancy_id)
+async def return_vacancy(session : AsyncSession, current_user : UserModel, vacancy_id : int):
+    return await find_owned_vacancy_or_404(session, current_user, vacancy_id)
 
-async def create_vacancy(session : AsyncSession, vacancy : VacancyCreate):
+async def create_vacancy(session : AsyncSession, current_user : UserModel, vacancy : VacancyCreate):
 
     new_vacancy = VacancyModel(company=vacancy.company, title = vacancy.title,
                                 url = str(vacancy.url) ,
                                status = vacancy.status,
-                                description = vacancy.description)
+                                description = vacancy.description,
+                               owner_id = current_user.id)
     session.add(new_vacancy)
     try:
         await session.commit()
@@ -51,9 +53,9 @@ async def create_vacancy(session : AsyncSession, vacancy : VacancyCreate):
 
 
 
-async def update_vacancy(session : AsyncSession, new_data: VacancyUpdate, vacancy_id: int):
+async def update_vacancy(session : AsyncSession, current_user : UserModel, new_data: VacancyUpdate, vacancy_id: int):
 
-    vacancy = await find_vacancy_or_404(session, vacancy_id)
+    vacancy = await find_owned_vacancy_or_404(session, current_user, vacancy_id)
     updates = new_data.model_dump(exclude_unset=True, mode="json")
     if not updates:
         raise HTTPException(status_code=400, detail="no updates were provided")
@@ -68,9 +70,9 @@ async def update_vacancy(session : AsyncSession, new_data: VacancyUpdate, vacanc
     return vacancy
 
 
-async def delete_vacancy(session : AsyncSession, vacancy_id : int):
+async def delete_vacancy(session : AsyncSession, current_user : UserModel, vacancy_id : int):
 
-    vacancy = await find_vacancy_or_404(session, vacancy_id)
+    vacancy = await find_owned_vacancy_or_404(session, current_user, vacancy_id)
     try:
         await session.delete(vacancy)
         await session.commit()
